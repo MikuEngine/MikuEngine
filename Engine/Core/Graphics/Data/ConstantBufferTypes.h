84d5b7f97890c4e9d586693f5200ed16a4a29a99
hth-hiro <hth0114@naver.com>
2026-01-19 00:36:53 +0900

    feat | UI관련 셰이더 코드 확장

--- Engine/Core/Graphics/Data/ConstantBufferTypes.h
+++ Engine/Core/Graphics/Data/ConstantBufferTypes.h
@@ -127,12 +127,14 @@ namespace engine
 
 	struct CbUIElement
 	{
-		Matrix clip;          // 최종 SV_Position용
-		Vector4 color;        // RGBA
-		Vector4 uv;           // offset.xy, scale.xy
-		Vector4 clipRect;     // (xMin, yMin, xMax, yMax) in pixels 또는 NDC 중 하나로 통일
-		uint32_t flags;       // useClip, isText 등
+		Matrix clip;        // 최종 SV_Position용
+		Vector4 color;      // RGBA
+		Vector4 uv;         // offset.xy, scale.xy
+		Vector4 clipRect;   // (xMin, yMin, xMax, yMax) in pixels 또는 NDC 중 하나로 통일
+		uint32_t maskMode;  // none, rect, circle, ring, rectring
 		float pad[3];
+
+		Vector4 mask0;		// 원형 프로그레스 바 전용
 	};
 
 }
