fe24b34be1a28d0c48b1b5281fcdea077b1d730d
DevOk <oki8370@naver.com>
2026-01-19 01:44:49 +0900

    feat | SoundSystem 세팅 추가

---
+++ Engine/Framework/System/SoundSystem.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "Framework/Object/Component/Component.h"
+//#include "fmod.hpp"
+//#include "fmod_errors.h"
+#include <filesystem>
+#include <map>
+#include <string>
+#include <vector>
+#include <list>
+#include <functional>
+#include <DirectXMath.h>
+
+using namespace DirectX;
+namespace fs = std::filesystem;
+using EventSoundEnd = std::function<void()>;
+
+//inline FMOD_VECTOR ToFmodVector(const XMFLOAT3& v) { return { v.x, v.y, v.z }; }
+
+namespace engine
+{
+
+
+
+}
