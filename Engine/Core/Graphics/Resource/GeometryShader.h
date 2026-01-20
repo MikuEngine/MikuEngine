8d35463d184bc3c2775446bb7aba95d1b1c8603b
sinik111 <kangmark95@naver.com>
2026-01-20 10:18:40 +0900

    feat | GeometryShader 추가

---
+++ Engine/Core/Graphics/Resource/GeometryShader.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Core/Graphics/Resource/Resource.h"
+
+namespace engine
+{
+	class GeometryShader :
+		public Resource
+	{
+	private:
+		Microsoft::WRL::ComPtr<ID3D11GeometryShader> m_geometryShader;
+
+	public:
+		void Create(const std::string& filePath);
+
+	public:
+		const Microsoft::WRL::ComPtr<ID3D11GeometryShader>& GetShader() const;
+		ID3D11GeometryShader* GetRawShader() const;
+		ID3D11GeometryShader* const* GetRawShaderAddr() const;
+	};
+}
