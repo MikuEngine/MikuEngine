da2062613ec1bd217d9bd96d035dce812b8744e7
rock-oon <jcgs0080@gmail.com>
2026-01-20 19:23:34 +0900

    fix | 에디터 하이어라키에서, 자식을 가진 부모 오브젝트는 삭제할 수 없도록 수정
    에디터에서 자식을 가진 부모 오브젝트를 삭제하면 런타임오류 발생함.
    에디터에서는 삭제가 안되도록 수정하고, 플레이 런타임에는 Destroy함수의 멤버에 자식 복사본으로 재귀 수행하도록 수정.
    
    플레이 런타임 디스트로이는 아직 테스트 못해봄.

---
+++ Game/TestGame/Script/TempMonster.cpp
@@ -0,0 +1,24 @@
+﻿#include "GamePCH.h"
+#include "TempMonster.h"
+
+namespace game
+{
+    void TempMonster::OnGui()
+    {
+    }
+
+    void TempMonster::Save(engine::json& j) const
+    {
+        Object::Save(j);
+    }
+
+    void TempMonster::Load(const engine::json& j)
+    {
+        Object::Load(j);
+    }
+
+    std::string TempMonster::GetType() const
+    {
+        return "TempMonster";
+    }
+}
