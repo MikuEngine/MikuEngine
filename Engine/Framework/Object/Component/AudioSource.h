8bc35aed91a284e009087fcd10c6fc933a23a803
okaybab <oki8370@naver.com>
2026-01-21 20:42:03 +0900

    feat | 사운드 페이드 인 /아웃 기능 추가

--- Engine/Framework/Object/Component/AudioSource.h
+++ Engine/Framework/Object/Component/AudioSource.h
@@ -10,6 +10,13 @@ namespace engine
     class SoundSystem;
     class SoundData;
 
+    enum class FadeState
+    {
+        None,
+        FadingIn,
+        FadingOut
+    };
+
     class AudioSource :
         public Component
     {
@@ -18,8 +25,8 @@ namespace engine
         friend class SoundSystem;
 
     private:
-        std::string m_clipName;             // 재생할 사운드 파일 키값
-        std::shared_ptr<SoundData> m_soundData;   // 로드된 사운드 리소스 데이터
+        std::string m_clipName;                 // 재생할 사운드 파일 키값
+        std::shared_ptr<SoundData> m_soundData;  // 로드된 사운드 리소스 데이터
 
         bool m_playOnAwake = false;
         bool m_isLoop = false;
@@ -30,6 +37,21 @@ namespace engine
         float m_minDistance = 1.0f;
         float m_maxDistance = 500.0f;
 
+        // Fade In / Out
+        FadeState m_fadeState = FadeState::None;
+        float m_fadeTimer = 0.0f;
+        float m_fadeDuration = 0.0f;
+        float m_startVolume = 0.0f;     // 페이드 시작 시점의 볼륨
+        float m_targetVolume = 0.0f;    // 페이드 목표 볼륨
+        float m_fadeInTime = 0.0f;      // 기본 페이드 인 시간
+        float m_fadeOutTime = 0.0f;     // 기본 페이드 아웃 시간
+
+        // Auto Stop
+        bool m_isAutoStop = false;
+        float m_sustainTimer = 0.0f;
+        float m_sustainDuration = 0.0f;
+        float m_scheduledFadeOutDuration = 0.0f;
+
         FMOD::Channel* m_currentChannel = nullptr;
 
     public:
@@ -37,11 +59,13 @@ namespace engine
         virtual ~AudioSource();
 
         void Initialize() override;
+        void Update();
         //void Awake() override;
         void OnDestroy() override;
 
-        void Play(EventEndPlay callback = nullptr);
-        void Stop();
+        void Play(EventEndPlay callback = nullptr, float fadeInDuration = 0.0f);
+        void Play(float fadeIn, float duration, float fadeOut);
+        void Stop(float fadeOutDuration = 0.0f);
         void LoadClipFromFile();
 
         void SetClip(std::string name); 
@@ -49,7 +73,9 @@ namespace engine
         void SetLoop(bool loop);
         void Set3D(bool enable);
         void SetForceStopState();
-        
+        void SetFadeInTime(float time) { m_fadeInTime = time; }
+        void SetFadeOutTime(float time) { m_fadeOutTime = time; }
+
         Sound* GetSoundResource() const;
         FMOD::Channel* GetChannel() const { return m_currentChannel; }
         bool Is3D() const { return m_is3D; }
