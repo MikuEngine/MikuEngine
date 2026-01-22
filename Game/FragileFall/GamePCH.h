c9d2be58852e51bc9bbe99da2060391d0c3697ae
sinik111 <kangmark95@naver.com>
2026-01-22 15:01:56 +0900

    feat | math random 추가 , particle emitter 추가

--- Game/FragileFall/GamePCH.h
+++ Game/FragileFall/GamePCH.h
@@ -4,17 +4,3 @@
 
 #include <Engine/Framework/Object/GameObject/GameObject.h>
 #include <Engine/Framework/Object/Component/Transform.h>
-#include <Engine/Framework/Object/Component/Renderer/SpriteRenderer.h>
-#include <Engine/Framework/Object/Component/Renderer/StaticMeshRenderer.h>
-#include <Engine/Framework/Object/Component/Renderer/SkeletalMeshRenderer.h>
-#include <Engine/Framework/Object/Component/Animator/SkeletalAnimator.h>
-#include <Engine/Framework/Object/Component/Animator/SpriteAnimator.h>
-#include <Engine/Framework/Object/Component/Camera.h>
-#include <Engine/Framework/Object/Component/Light.h>
-
-// Physics Components
-#include <Engine/Framework/Object/Component/Rigidbody.h>
-#include <Engine/Framework/Object/Component/BoxCollider.h>
-#include <Engine/Framework/Object/Component/SphereCollider.h>
-#include <Engine/Framework/Object/Component/CapsuleCollider.h>
-#include <Engine/Framework/Object/Component/CharacterController.h>
