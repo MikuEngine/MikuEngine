a95374d85b0b3b0ea20031e12dabe8b6cbf6b2b5
hth-hiro <hth0114@naver.com>
2026-01-26 14:02:02 +0900

    refactor | 스크립트 네이밍 변경 및 씬에 스크립트 추가
    기존에 썼었던 모호한 스크립트 이름을 변경했습니다.
    
    변경 전:
    TitleButtonController
    HubButtonController
    PlayButtonController
    
    변경 후:
    SceneController_Main
    SceneController_Lobby
    SceneController_Play

---
+++ Game/FragileFall/Script/UIPopUpAnimator.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include <Framework/Object/Component/Script.h>
+
+namespace game
+{
+    class UIPopUpAnimator :
+        public engine::Script<UIPopUpAnimator>
+    {
+        REGISTER_COMPONENT(UIPopUpAnimator, Script)
+
+    public:
+        void Awake() override;
+        void Start() override;
+        void Update() override;
+
+    public:
+        void OnGui() override;
+        void Save(engine::json& j) const override;
+        void Load(const engine::json& j) override;
+
+        void Open();
+        void Close();
+
+    private:
+        engine::RectTransform* m_rt = nullptr;
+
+        engine::Vector2 m_visible{ 0.0f, 200.0f };
+        engine::Vector2 m_hidden{ 0.0f, 900.0f };
+
+        float m_time = 0.0f;
+        float m_duration = 0.25f;
+
+        bool m_isOpen = false;
+        bool m_animating = false;
+        bool m_opening = false;
+
+        bool m_inited = false;
+    };
+}
---
+++ Game/FragileFall/Script/UpgradeNodeView.cpp
@@ -0,0 +1,154 @@
+﻿#include "GamePCH.h"
+#include "UpgradeNodeView.h"
+#include "Framework/Object/Component/UI/UIClickArea.h"
+#include "UpgradeSystem.h"
+namespace game
+{
+    void UpgradeNodeView::Awake()
+    {
+        auto* go = GetGameObject();
+        if (!go) return;
+
+        auto* click = go->GetComponent<engine::UIClickArea>();
+        if (!click) return;
+
+        auto* sysGo = engine::GameObject::Find("UpgradeSystem");
+        if (!sysGo) return;
+
+        auto* sys = sysGo->GetComponent<game::UpgradeSystem>();
+        if (!sys) return;
+
+        click->AddOnClick([this, sys](int mouseButton)
+            {
+                if (mouseButton != 0) return;
+
+                if (sys)
+                    sys->ApplyUpgrade(m_nodeId);
+            });
+    }
+
+    void UpgradeNodeView::OnGui()
+    {
+        ImGui::InputInt("NodeId", &m_nodeId);
+
+
+
+        ImGui::Text("Parents");
+        for (int i = 0; i < (int)m_parents.size(); ++i)
+        {
+            ImGui::PushID(i);
+
+            int v = m_parents[i];
+            if (ImGui::InputInt("##Parent", &v))
+            {
+                // 자기 자신 금지
+                if (v != m_nodeId)
+                {
+                    // 중복 방지
+                    bool dup = false;
+                    for (int k = 0; k < (int)m_parents.size(); ++k)
+                    {
+                        if (k != i && m_parents[k] == v)
+                        {
+                            dup = true;
+                            break;
+                        }
+                    }
+
+                    if (!dup)
+                        m_parents[i] = v;
+                }
+            }
+
+            ImGui::SameLine();
+            if (ImGui::Button("X"))
+            {
+                m_parents.erase(m_parents.begin() + i);
+                ImGui::PopID();
+                break; // erase 이후 바로 탈출
+            }
+
+            ImGui::PopID();
+        }
+
+        ImGui::Separator();
+
+
+        ImGui::InputInt("New Parent", &m_newParent);
+
+        bool canAdd = (m_newParent != m_nodeId);
+        for (int pid : m_parents)
+        {
+            if (pid == m_newParent)
+            {
+                canAdd = false;
+                break;
+            }
+        }
+
+        if (!canAdd) ImGui::BeginDisabled(true);
+        if (ImGui::Button("+ Add Parent"))
+        {
+            m_parents.push_back(m_newParent);
+            m_newParent = 0;
+        }
+        if (!canAdd) ImGui::EndDisabled();
+
+
+
+
+
+
+        ImGui::InputText("Name", &m_name);
+        ImGui::InputTextMultiline("Desc", &m_desc);
+
+        ImGui::InputInt("Ruby", &m_ruby);
+        ImGui::InputInt("Sapphire", &m_sapphire);
+        ImGui::InputInt("Emerald", &m_emerald);
+    }
+
+    void UpgradeNodeView::Save(engine::json& j) const
+    {
+        Object::Save(j);
+
+        j["NodeId"] = m_nodeId;
+
+        engine::json parents = engine::json::array();
+        for (int pid : m_parents)
+            parents.push_back(pid);
+        j["Parent"] = parents;
+
+        j["Name"] = m_name;
+        j["Desc"] = m_desc;
+
+        j["Ruby"] = m_ruby;
+        j["Sapphire"] = m_sapphire;
+        j["Emerald"] = m_emerald;
+    }
+
+    void UpgradeNodeView::Load(const engine::json& j)
+    {
+        Object::Load(j);
+
+        engine::JsonGet(j, "NodeId", m_nodeId);
+
+        m_parents.clear();
+        engine::JsonArrayForEach(j, "Parent",
+            [this](const engine::json& v)
+            {
+                m_parents.push_back(v.get<int>());
+            });
+
+        engine::JsonGet(j, "Name", m_name);
+        engine::JsonGet(j, "Desc", m_desc);
+
+        engine::JsonGet(j, "Ruby", m_ruby);
+        engine::JsonGet(j, "Sapphire", m_sapphire);
+        engine::JsonGet(j, "Emerald", m_emerald);
+    }
+
+    void UpgradeNodeView::SetVisualState(bool unlocked, bool purchased)
+    {
+        //
+    }
+}
