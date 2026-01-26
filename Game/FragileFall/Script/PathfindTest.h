d8466c0ea9f11db53353a8a2381cc3b004d9ebc7
sinik111 <kangmark95@naver.com>
2026-01-26 19:19:11 +0900

    feat | 프로토타입 작업중

---
+++ Game/FragileFall/Script/PathfindTest.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include <Framework/Object/Component/Script.h>
+
+namespace engine
+{
+    class PathfindingAgent;
+    class GameObject;
+}
+
+namespace game
+{
+    class PathfindTest :
+        public engine::Script<PathfindTest>
+    {
+        REGISTER_SCRIPT(PathfindTest, Script)
+
+    private:
+        engine::PathfindingAgent* m_agent = nullptr;
+        engine::GameObject* m_target = nullptr;
+        float m_moveSpeed = 3.0f;
+
+    public:
+        //void Awake() override;
+        void Start() override;
+        void Update() override;
+
+    public:
+        void OnGui() override;
+        void Save(engine::json& j) const override;
+        void Load(const engine::json& j) override;
+    };
+}
