417d9a06ed5677083fc447229e80a3ae9f1a796b
hth-hiro <hth0114@naver.com>
2026-01-27 16:57:27 +0900

    feat | 씬에 버튼 애니메이션 추가

---
+++ Game/FragileFall/Script/UIHoverScale.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include <Framework/Object/Component/Script.h>
+
+namespace engine
+{
+    class RectTransform;
+    class UIButton;
+}
+
+namespace game
+{
+    class UIHoverScale :
+        public engine::Script<UIHoverScale>
+    {
+        REGISTER_SCRIPT(UIHoverScale, Script)
+
+    public:
+        void Awake() override;
+        //void Start() override;
+        void Update() override;
+
+    public:
+        void OnGui() override;
+        void Save(engine::json& j) const override;
+        void Load(const engine::json& j) override;
+
+    private:
+        void Apply();
+
+    private:
+        engine::RectTransform* m_rt = nullptr;
+        engine::UIButton* m_btn = nullptr;
+        engine::Vector2 m_normalScale = { 1.0f, 1.0f };
+        engine::Vector2 m_baseSize = { 100.0f, 100.0f };
+
+        float m_hoverScale = 1.1f;
+
+        float m_speed = 12.0f;
+
+        bool m_hovered = false;
+    };
+}
