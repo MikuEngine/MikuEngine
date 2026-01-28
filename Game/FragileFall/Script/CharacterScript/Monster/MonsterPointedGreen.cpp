6c376464804bd1702f739429e36be4694666ec1e
rock-oon <jcgs0080@gmail.com>
2026-01-28 11:18:44 +0900

    fix | 콜리전 디버그 렌더러의 런타임 오류 수정
    콜리전 디버그 렌더러에서도 핸들 기반 타입유효성 체크하도록함.

--- Game/FragileFall/Script/CharacterScript/Monster/MonsterPointedGreen.cpp
+++ Game/FragileFall/Script/CharacterScript/Monster/MonsterPointedGreen.cpp
@@ -1,4 +1,4 @@
-#include "GamePCH.h"
+﻿#include "GamePCH.h"
 #include "MonsterPointedGreen.h"
 
 #include "Script/CharacterScript/Common/BulletFactory.h"
@@ -21,9 +21,9 @@ namespace game
 
         // PointedGreen 고유 스탯 설정
         m_Hp = 10;
-        m_AttackRange = 7.0f;
+        m_AttackRange = 2.0f;
         m_detectionRange = 40.0f;
-        m_moveSpeed = 0.3f;  // 이동 가능
+        m_moveSpeed = 5.0f;  // 이동 가능
         m_rotationSpeed = 3.0f * 3.14159f;  // 360도/초 (라디안)
         m_fireRate = 3.0f;
         m_fireTimer = m_fireRate;
