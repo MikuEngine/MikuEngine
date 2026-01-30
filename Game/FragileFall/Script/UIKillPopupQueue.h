730551da60c3c8ac7d5304baa519ad4385690c2b
hth-hiro <hth0114@naver.com>
2026-01-30 12:26:13 +0900

    feat | UIToastAnimator 추가

---
+++ Game/FragileFall/Script/UIKillPopupQueue.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <Framework/Object/Component/Script.h>
+
+namespace game
+{
+    class UIKillPopupQueue :
+        public engine::Script<UIKillPopupQueue>
+    {
+        REGISTER_SCRIPT(UIKillPopupQueue, Script)
+
+    public:
+        void Awake() override;
+        void Start() override;
+        void Update() override;
+
+    public:
+        void OnGui() override;
+        void Save(engine::json& j) const override;
+        void Load(const engine::json& j) override;
+
+    private:
+        //void PushKill(const std::string& text);
+
+    private:
+        engine::GameObject* m_container = nullptr;
+        engine::GameObject* m_prefab = nullptr;
+        engine::GameObject* m_canvas = nullptr;
+
+        float m_spacing = 6.0f;
+        float m_lifeTime = 1.2f;
+        int m_maxQueue = 6;
+    };
+}
