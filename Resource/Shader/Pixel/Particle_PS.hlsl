f8261e7295b39d4c29056dc71b1afaf5bc095e47
sinik111 <kangmark95@naver.com>
2026-02-02 17:32:36 +0900

    fix | emissive 곱셈으로 변경

--- Resource/Shader/Pixel/Particle_PS.hlsl
+++ Resource/Shader/Pixel/Particle_PS.hlsl
@@ -5,5 +5,5 @@ float4 main(PS_INPUT_PARTICLE input) : SV_TARGET
     float4 texColor = g_texBaseColor.Sample(g_samLinear, input.texCoord);
     float4 base = texColor * input.color;
     float3 emissive = input.emissiveColor * input.emissiveIntensity;
-    return float4(base.rgb + emissive, base.a);
+    return float4(base.rgb * emissive, base.a);
 }
