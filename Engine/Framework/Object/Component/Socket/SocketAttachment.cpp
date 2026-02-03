39df7d846e0e1ba50d6b6d74cfccb61c9999f798
okaybab <oki8370@naver.com>
2026-02-03 15:33:29 +0900

    fix | Script에서 Component로 변경

--- Engine/Framework/Object/Component/Socket/SocketAttachment.cpp
+++ Engine/Framework/Object/Component/Socket/SocketAttachment.cpp
@@ -34,31 +34,4 @@ namespace engine
     {
         m_targetRenderer = nullptr;
     }
-
-    void SocketAttachment::OnGui()
-    {
-        Script::OnGui();
-        ImGui::Text("Target Renderer: %s", m_targetRenderer ? m_targetRenderer->GetGameObject()->GetName().c_str() : "None");
-
-        char buffer[256];
-        strcpy_s(buffer, m_socketName.c_str());
-        if (ImGui::InputText("Socket Name", buffer, sizeof(buffer)))
-        {
-            m_socketName = buffer;
-        }
-    }
-
-    void SocketAttachment::Save(json& j) const
-    {
-        Script::Save(j);
-
-        j["SocketName"] = m_socketName;
-    }
-
-    void SocketAttachment::Load(const json& j)
-    {
-        Script::Load(j);
-
-        JsonGet(j, "SocketName", m_socketName);
-    }
 }
