a2814675229f8136b680d51d179e38b3459577fd
okaybab <oki8370@naver.com>
2026-02-06 14:35:07 +0900

    chore | system key F10 위치 옮김

--- Engine/Core/App/WinApp.cpp
+++ Engine/Core/App/WinApp.cpp
@@ -541,6 +541,12 @@ namespace engine
         case WM_SYSKEYUP:
             DirectX::Keyboard::ProcessMessage(uMsg, wParam, lParam);
             break;
+        case WM_SYSKEYDOWN:
+            if (wParam == VK_F10)
+            {
+                DirectX::Keyboard::ProcessMessage(uMsg, wParam, lParam);
+            }
+            break;
 
         default:
             return DefWindowProcW(hWnd, uMsg, wParam, lParam);
@@ -765,12 +771,6 @@ namespace engine
 
     bool WinApp::HandleImGuiInput(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
-        if (uMsg == WM_SYSKEYDOWN && wParam == VK_F10)
-        {
-            DirectX::Keyboard::ProcessMessage(uMsg, wParam, lParam);
-            return true;
-        }
-
 #ifdef _DEBUG
         // 마우스 좌표가 포함된 메시지인지 확인 (휠 제외)
         bool isMouseCoordMsg = false;
