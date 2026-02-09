a11fc7d1a4981bd1f603b34ddab1099ba97b2726
hth-hiro <hth0114@naver.com>
2026-02-09 19:57:39 +0900

    feat | UISlider 밸류값 텍스트 연동 추가

---
+++ Game/FragileFall/Script/UI/UISliderValueTextBinder.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <Framework/Object/Component/Script.h>
+
+namespace engine
+{
+    class UIText;
+}
+
+namespace game
+{
+    class UISliderValueTextBinder :
+        public engine::Script<UISliderValueTextBinder>
+    {
+        REGISTER_SCRIPT(UISliderValueTextBinder, Script)
+
+    public:
+        void Awake() override;
+        void Start() override;
+        void Update() override;
+
+    public:
+        void OnGui() override;
+        void Save(engine::json& j) const override;
+        void Load(const engine::json& j) override;
+
+    private:
+        void Refresh(bool force);
+
+    private:
+        engine::UIText* m_volumeTxt = nullptr;
+        engine::UIText* m_SFXTxt = nullptr;
+        engine::UIText* m_SensiTxt = nullptr;
+    };
+}
