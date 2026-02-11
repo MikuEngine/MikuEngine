e67ba8fd61f004b9e17208f2084cfbda7e71228c
sinik111 <kangmark95@naver.com>
2026-02-11 15:21:11 +0900

    feat | 리소스 업로드

--- Resource/Shader/Pixel/UIQuad_PS.hlsl
+++ Resource/Shader/Pixel/UIQuad_PS.hlsl
@@ -509,7 +509,6 @@ float4 main(PS_INPUT_TEXCOORD input) : SV_Target
     
     // 3. 메인 텍스처 샘플링
     float4 tex = g_texBlit.Sample(g_samLinear, uv);
-    tex.rgb = SRGBToLinear(tex.rgb);
     finalColor *= tex;
 
     // 4. 색상/알파 기반 이펙트 (텍스처 샘플링 후)
